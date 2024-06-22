a9d584006c997cfd4f7182e6e308eab01b8c5bcc
klaudiamilosia <klaudiamiloszewska100@gmail.com>
2024-06-22 14:51:47 +0200

    locations modification

--- client/weatherclient/src/components/MenuLocationForecast.jsx
+++ client/weatherclient/src/components/MenuLocationForecast.jsx
@@ -3,14 +3,20 @@ import { isEmpty } from '../utils/isEmpty';
 import { weatherAPI } from '../utils/baseURL';
 import useRetrieveForecast from '../hooks/useRetrieveForecast';
 import { useMenuStore } from '../store/menuStore';
+import { FaMinus } from "react-icons/fa6";
+import useLocationStore from '../store/locationStore';
 
-const MenuLocationForecast = ({item}) => {
+const MenuLocationForecast = ({item, managementMode}) => {
     const [forecast, setForecast] = useState({})
     const toggleMenu = useMenuStore((state) => state.toggleMenu)
+    const removeLocation = useLocationStore((state) => state.removeLocation)
     const { fetchForecastByCity } = useRetrieveForecast()
     const handleChangeCurrentLocation = (cityName) => {
         fetchForecastByCity(cityName)
         toggleMenu()
+    }
+    const handleRemoveLocation = () => {
+        removeLocation(item)
     }
    
     useEffect(() => {
@@ -29,12 +35,16 @@ const MenuLocationForecast = ({item}) => {
         fetchForecast(item)
     },[])
   return (
-    <li onClick={() => {handleChangeCurrentLocation(item)}} className='rounded-md bg-slate-800 py-6 px-5 font-light text-xl flex justify-between items-center my-2 hover:bg-slate-700 cursor-pointer'>
-        <h1 className='font-extralight'>{item}</h1>
-        {!isEmpty(forecast) &&
-        <p className='font-medium'>{forecast.current.temp_c} °C</p>
-        }
+    <li className='flex w-full my-2 items-center gap-3'>
+        <div onClick={() => {handleChangeCurrentLocation(item)}} className='w-full rounded-md bg-slate-800 py-6 px-5 font-light text-xl flex justify-between items-center hover:bg-slate-700 cursor-pointer'>
+            <h1 className='font-extralight'>{item}</h1>
+            {!isEmpty(forecast) &&
+            <p className='font-medium'>{forecast.current.temp_c} °C</p>
+            }
+        </div>
+        {managementMode && <button onClick={handleRemoveLocation} className='rounded-full p-2 bg-slate-800 h-max hover:bg-slate-700'><FaMinus size={14} /></button>}
     </li>
+    
   )
 }
 
